63ed062b0f13fa4eba1c3e1ef00a36f5d1c16cea
Rewina <rewinazerou6@gmail.com>
2023-06-03 23:28:40 +0300

    allowed url changed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "file-examples.com/",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
