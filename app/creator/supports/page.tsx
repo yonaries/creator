ff833fd2736aed951ec2fee79edf41c38888140f
Rewina <rewinazerou6@gmail.com>
2023-06-10 16:58:24 +0300

    testing data table component

--- app/creator/supports/page.tsx
+++ app/creator/supports/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import TabBar from "@/components/tabbar";
 import { Separator } from "@/components/ui/separator";
 import Benefits from "./components/benefits";
@@ -5,8 +7,120 @@ import BlockedUsers from "./components/blocked-users";
 import Donations from "./components/donations";
 import ExitSurvey from "./components/exit-survey";
 import Supporters from "./components/supporters";
+import { DataTable } from "@/components/data-table";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/data-table-column-header";
 
 type Props = {};
+
+const data = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728asded52f",
+    amount: 101,
+    status: "paid",
+    email: "am@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728asded52f",
+    amount: 101,
+    status: "paid",
+    email: "am@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728asded52f",
+    amount: 101,
+    status: "paid",
+    email: "am@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728asded52f",
+    amount: 101,
+    status: "paid",
+    email: "am@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728asded52f",
+    amount: 101,
+    status: "paid",
+    email: "am@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
+
+const columns: ColumnDef<{
+  id: string;
+  status: string;
+  email: string;
+  amount: number;
+}>[] = [
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Email" />
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Amount" />
+    ),
+  },
+];
+
+const statusOptions = [
+  {
+    value: "paid",
+    label: "paid",
+  },
+  {
+    value: "pending",
+    label: "pending",
+  },
+];
 
 const Supports = (props: Props) => {
   const tabBarItems = {
@@ -27,9 +141,23 @@ const Supports = (props: Props) => {
   };
   return (
     <div>
-      <span className="text-3xl font-bold">Supporters</span>
       <Separator className="my-3" />
       <TabBar items={tabBarItems} />
+
+      <DataTable
+        facetFilters={[
+          {
+            title: "Status",
+            column: "status",
+            options: statusOptions,
+          },
+        ]}
+        columns={columns}
+        data={data}
+        filterColumnName="email"
+        title="Supporters"
+        caption="This is supporters list."
+      />
     </div>
   );
 };
