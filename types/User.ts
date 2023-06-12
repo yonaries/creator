ff58b02f04f940ccecb0430fa9c889673c1f7a20
Rewina <rewinazerou6@gmail.com>
2023-06-13 00:02:45 +0300

    user type created

---
+++ types/User.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  displayName: string;
+  email: string;
+  status: string;
+  residence: string;
+  createdAt: string;
+  updatedAt: string;
+  firstName?: string;
+  lastName?: string;
+  profileImage?: string;
+}
