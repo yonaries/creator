838f1e56fd61310119aae99363fd3244e5f2c88c
yonaries <yonaries.dev@gmail.com>
2023-06-23 00:53:56 +0300

    props missing fixed

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -1,11 +1,14 @@
+"use client";
+import { useAuth } from "../context/auth-context";
 import CoverImage from "./dashboard/components/cover-image";
 import DashboardHeader from "./dashboard/components/dashboard-header";
 import PageContents from "./dashboard/components/page-contents";
 
 export default function Dashboard() {
+  const { currentUserPage } = useAuth();
   return (
     <div>
-      <CoverImage />
+      <CoverImage src={currentUserPage?.coverImage} />
       <div className="flex w-full flex-col justify-center p-10">
         <DashboardHeader />
         <PageContents />
