14b0b2c66c59c64a15aba3bb569cfbccb4b315c7
Rewina <rewinazerou6@gmail.com>
2023-06-27 13:56:10 +0300

    subscription works

--- app/(supporters-portal)/account/components/post-list.tsx
+++ app/(supporters-portal)/account/components/post-list.tsx
@@ -32,7 +32,7 @@ const PostsList = (props: Props) => {
   return (
     <div className="flex flex-col items-center">
       {data.map((item) => (
-        <PostCard key={item.id} post={item}>
+        <PostCard posts={data} key={item.id} post={item}>
           <div className="flex items-center space-x-3 px-4 py-2">
             <Avatar className="w-10">
               <AvatarImage src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/465.png" />
